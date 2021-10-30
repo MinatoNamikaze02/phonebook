4094b95c7e75fd393e2dc4dd385b0e71c60b22db
Arjun <arjunsreedar@yahoo.com>
2021-10-30 15:45:46 +0530

    main commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const morgan = require('morgan')
-
+app.use(express.static('build'))
 app.use(express.json());
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })   // returns body for logging
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
